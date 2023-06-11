02620097490c09e73fc7274e51da98fd76c4a6ce
yabe-k <136215962+yabe-k@users.noreply.github.com>
2023-06-11 13:14:44 +0900

    Add files via upload
    First commit

---
+++ camera.js
@@ -0,0 +1,15 @@
+function captureImage(video){
+    const canvas = document.getElementById("picture")
+    const soundFile = "./sound/sound2.mp3";
+    const audio = new Audio(soundFile);
+    audio.play();
+    const fileName = "capture_" + getTime() + ".png";
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(video, 0, 0);
+}
+
+function getTime(){
+    now = new Date();
+    return now.getFullYear() + "-" + ("00" + String(Number(now.getMonth()) + 1)).slice(-2) + "-" + ("00" + now.getDate()).slice(-2) + "_" + ("00" + now.getHours()).slice(-2) + "-" + ("00" + now.getMinutes()).slice(-2)+ "-" + ("00" + now.getSeconds()).slice(-2)
+}
+
