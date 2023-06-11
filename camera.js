3a342e30486894304412e2c8984b65ce74d2ae48
yabe-k <136215962+yabe-k@users.noreply.github.com>
2023-06-11 16:28:55 +0900

    Add files via upload

--- camera.js
+++ camera.js
@@ -12,3 +12,29 @@ function getTime(){
     now = new Date();
     return now.getFullYear() + "-" + ("00" + String(Number(now.getMonth()) + 1)).slice(-2) + "-" + ("00" + now.getDate()).slice(-2) + "_" + ("00" + now.getHours()).slice(-2) + "-" + ("00" + now.getMinutes()).slice(-2)+ "-" + ("00" + now.getSeconds()).slice(-2)
 }
+
+function playVideo(){
+    const video = document.getElementById("video");
+    var selectedId = document.getElementById("select-camera").value;
+    var video_height = document.getElementById("video-height").value;
+    var video_width = document.getElementById("video-width").value;
+    //console.log(selectedId);
+    navigator.mediaDevices.getUserMedia({
+        video: {
+            deviceId: selectedId,
+            width: video_width,
+            height: video_height,
+        },
+        audio: false,
+    }).then(stream => {
+        video.srcObject = stream;
+        video.play()
+    }).catch(e => {
+        console.log(e)
+    }).finally(() => {
+        var canvas = document.getElementById("picture")
+        var w = video.clientWidth;
+        var h = video.clientHeight;
+        
+    })
+}
