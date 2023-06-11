e1527c5b974d1512a84cfbb9d721bd887fd984b2
yabe-k <136215962+yabe-k@users.noreply.github.com>
2023-06-11 14:27:51 +0900

    Add files via upload

--- camera.js
+++ camera.js
@@ -12,11 +12,3 @@ function getTime(){
     now = new Date();
     return now.getFullYear() + "-" + ("00" + String(Number(now.getMonth()) + 1)).slice(-2) + "-" + ("00" + now.getDate()).slice(-2) + "_" + ("00" + now.getHours()).slice(-2) + "-" + ("00" + now.getMinutes()).slice(-2)+ "-" + ("00" + now.getSeconds()).slice(-2)
 }
-
-function addOption(name, id) {
-    var select = document.getElementById("select-camera");
-    var option = document.createElement("option");
-    option.text = name;
-    option.value = id;
-    select.appendChild(option);
-}
